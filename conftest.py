f6d73871d177c3fb48624cdc27a8bbca52274aef
Эмиль Валиуллин <valiullin.dds@gmail.com>
2022-11-09 12:29:32 +0300

    Editing

--- conftest.py
+++ conftest.py
@@ -4,12 +4,14 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.firefox.options import Options
 
 '''Данная фикстура применяется, если нужно провести тест на разных браузерах. По умолчанию установлен Chrome.
-Для теста в Firefox необходимо передать параметр --browser_name=firefox'''
+Для теста в Firefox необходимо передать параметр --browser_name=firefox. Оставляю ее в закомментированном виде,
+Если установлен бразуер Firefox и есть geckodriver можно ее раскомментировать и проверить тест на браузере Firefox
+предварительно закомментировав вторую фикстуру.'''
 
-'''Сами драйвера для Chrome и Firefox добавлены в системны XPATH, поэтому я не передаю путь к ним в самой фикстуре.'''
+'''Сами драйвера для Chrome и Firefox добавлены в системны Path.'''
 
-
-def pytest_addoption(parser):
+# # Фикстура открытия и закрытия браузера Chrome или Firefox
+'''def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default='chrome',
                      help="Choose browser: chrome or firefox")
 
@@ -20,16 +22,22 @@ def browser(request):
     browser = None
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
-        chrome_options = Options()
-        chrome_options.add_argument('--disable-notifications')
-        browser = webdriver.Chrome(options=chrome_options)
+        browser = webdriver.Chrome()
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
-        firefox_options = Options()
-        firefox_options.add_argument('--disable-notifications')
-        browser = webdriver.Firefox(options=firefox_options)
+        browser = webdriver.Firefox()
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
     print("\nquit browser..")
+    browser.quit()'''
+
+
+# Фикстура открытия и закрытия браузера Chrome
+@pytest.fixture(scope='function')
+def browser(request):
+    print('\nstart browser for test..')
+    browser = webdriver.Chrome() 
+    yield browser
+    print('\nquit browser..')
     browser.quit()
